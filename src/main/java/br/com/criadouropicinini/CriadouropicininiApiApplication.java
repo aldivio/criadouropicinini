f4a1c916702333bdcd84d48339b250a3922808b1
Aldivio <aldivio@microtop.com.br>
2024-06-25 10:42:31 -0300

    Primeiro commit

---
+++ src/main/java/br/com/criadouropicinini/CriadouropicininiApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.criadouropicinini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriadouropicininiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriadouropicininiApiApplication.class, args);
+	}
+
+}
